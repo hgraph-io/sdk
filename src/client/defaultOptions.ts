bdf639558ec1d6f8ccf640186bcc53c314109289
splash <splash@hgraph.io>
2023-04-13 10:08:15 -0600

    unathenticated access for testing

--- src/client/defaultOptions.ts
+++ src/client/defaultOptions.ts
@@ -1,13 +1,13 @@
-import {Network} from '../types'
+// import {Network} from '../types'
 
 /*
  * headers or client specific options
  */
 export default {
-  endpoint: 'https://api.hgraph.dev/v1/graphql', //TODO: change to production endpoint
+  endpoint: 'https://testnet.api.hgraph.dev/v1/graphql', //TODO: change to production endpoint
   headers: {
     'content-type': 'application/json',
-    'x-hgraph-network': Network.HederaMainnet,
+    // 'x-hgraph-network': Network.HederaMainnet,
     // 'x-hgraph-network': Network.HederaTestnet,
   },
 }
