43f1a93f2b995aeaae7c265e1aa44ec32188a33f
Deligrim <unidevc@ya.ru>
2024-05-29 15:55:32 +0300

    HG-22 Unable to summarize

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,5 +1,7 @@
 import fetch from 'isomorphic-fetch'
 import WebSocket from 'isomorphic-ws'
+import { createPatch } from 'rfc6902';
+import { AddOperation, RemoveOperation, ReplaceOperation } from 'rfc6902/diff';
 import {createClient, Client as SubscriptionClient} from '../../graphql-ws/src'
 import {
   Network,
@@ -8,8 +10,10 @@ import {
   Client,
   FlexibleRequestBody,
   SubscriptionHandlers,
+  PatchedSubscriptionHandlers,
+  PatchOperation,
 } from '../types'
-import {formatRequestBody} from './utils'
+import { formatRequestBody, resolveJsonPointer } from './utils'
 
 // generate types
 //https://github.com/evanw/esbuild/issues/95#issuecomment-1007485134
@@ -77,5 +81,31 @@ export default class HgraphClient implements Client {
   ) {
     const body = formatRequestBody(flexibleRequestBody)
     return this.subscriptionClient.subscribe(body, handlers)
+  }
+
+  patchedSubscribe(
+    flexibleRequestBody: FlexibleRequestBody,
+    handlers: PatchedSubscriptionHandlers
+  ) {
+    let prevData = null;
+    const body = formatRequestBody(flexibleRequestBody)
+    return this.subscriptionClient.subscribe(body, {
+      ...handlers,
+      next: (data) => {
+        let patches: PatchOperation[] = [];
+        if (prevData) {
+          patches = createPatch(prevData, data)
+            .map((operation: AddOperation | ReplaceOperation | RemoveOperation) => {
+              return {
+                ...operation,
+                //add value to remove operation
+                value: operation.op == 'remove' ? resolveJsonPointer(prevData, operation.path) : operation.value
+              }
+            });
+        }
+        prevData = data;
+        handlers.next(data, patches);
+      },
+    })
   }
 }
