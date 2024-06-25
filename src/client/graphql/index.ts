ec5a6e7bd9aaec45083eb748dbb80803859f5374
Deligrim <unidevc@ya.ru>
2024-06-25 13:07:03 +0300

    HG-102 Add a query to the SDK for future reference

--- src/client/graphql/index.ts
+++ src/client/graphql/index.ts
@@ -1,2 +1,3 @@
 export {default as AccountTransfers} from './AccountTransfers.gql'
 export {default as ContractEventLogs} from './ContractEventLogs.gql'
+export {default as RemainingAutomaticTokenAssociations} from './RemainingAutomaticTokenAssociations.gql'
